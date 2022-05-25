f6390141a4dbd633f208fb52bf367827f9ef875f
Sanoj Shrestha <sanoj.shrestha.13@gmail.com>
2022-05-25 19:05:20 +0545

    Update readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Task for avocado internship
+There are 2 models Genre and Video
+Genre model has 2 field name and description
+Video model has 4 field name, description, genre_id, active,link
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 <body>
     <div class="container">
         <div class="row mt-5">
-            <div class="col-md-2"></div>
+            <div class="col-md-2 col-sm-2"></div>
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
